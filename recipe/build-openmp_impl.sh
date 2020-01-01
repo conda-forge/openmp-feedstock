fa613d0850f070b894b78cbbe24c75070f1c5e05
Matthew R Becker <becker.mr@gmail.com>
2020-01-01 10:09:48 -0600

    more output for debugging

--- recipe/build-openmp_impl.sh
+++ recipe/build-openmp_impl.sh
@@ -3,3 +3,6 @@ mkdir -p $PREFIX/lib
 echo "Linking 'libgomp${SHLIB_EXT}.1' to 'libomp${SHLIB_EXT}'"
 
 ln -s $PREFIX/lib/libomp${SHLIB_EXT} libgomp${SHLIB_EXT}.1
+
+echo "OpenMP libs:"
+ls -1 ${PREFIX}/lib/lib*omp*
