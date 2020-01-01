81ef0c3adc86a6bb3efd2d6e8de33d2ec18fe951
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-01-01 11:33:25 -0600

    Update build-openmp_impl.sh

--- recipe/build-openmp_impl.sh
+++ recipe/build-openmp_impl.sh
@@ -2,4 +2,7 @@ mkdir -p $PREFIX/lib
 
 echo "Linking 'libgomp${SHLIB_EXT}.1' to 'libomp${SHLIB_EXT}'"
 
-ln -s $PREFIX/lib/libomp${SHLIB_EXT} $PREFIX/lib/libgomp${SHLIB_EXT}.1
+pushd $PREFIX/lib
+ln -s libomp${SHLIB_EXT} libgomp${SHLIB_EXT}.1
+ls -lah
+popd
