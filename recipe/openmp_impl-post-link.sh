bbcfd6bb0b138f92471a8646d189182b6c9d06ec
Matthew R Becker <becker.mr@gmail.com>
2020-01-04 09:41:55 -0600

    try again

--- recipe/openmp_impl-post-link.sh
+++
@@ -1,8 +0,0 @@
-
-if [ ! -f "$PREFIX/lib/libgomp.so.1" ]; then
-    ln -s $PREFIX/lib/libomp.so $PREFIX/lib/libgomp.so.1
-else
-    echo "\
-${PKG_NAME}-${PKG_VERSION}: \$PREFIX/lib/libgomp.so.1 \
-already exists so it was not symlinked to \$PREFIX/lib/libomp.so!" >> $PREFIX/.messages.txt
-fi
--- recipe/openmp_impl-pre-unlink.sh
+++
@@ -1,9 +0,0 @@
-
-if [ "$PREFIX/lib/libgomp.so.1" -ef "$PREFIX/lib/libomp.so" ]; then
-    # remove the file if it is a link
-    rm $PREFIX/lib/libgomp.so.1
-else
-    echo "\
-${PKG_NAME}-${PKG_VERSION}: \$PREFIX/lib/libgomp.so.1 \
-is not a symlink to \$PREFIX/lib/libomp.so so it was not removed!" >> $PREFIX/.messages.txt
-fi
