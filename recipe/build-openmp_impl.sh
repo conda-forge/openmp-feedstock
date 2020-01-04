ee1d49d896175a223e0aa69854389d4b7a7f53f9
Matthew R Becker <becker.mr@gmail.com>
2020-01-04 09:31:24 -0600

    try llvm in host

--- recipe/build-openmp_impl.sh
+++ recipe/build-openmp_impl.sh
@@ -1,17 +1,17 @@
 # TODO remove with conda build > 3.18.11
 # hack to make conda build happy
-mkdir -p $PREFIX/bin
-for action in post-link pre-unlink; do
-    cp $RECIPE_DIR/openmp_impl-${action}.sh $PREFIX/bin/.openmp_impl-${action}.sh
-done
+# mkdir -p $PREFIX/bin
+# for action in post-link pre-unlink; do
+#     cp $RECIPE_DIR/openmp_impl-${action}.sh $PREFIX/bin/.openmp_impl-${action}.sh
+# done
 # end of hack
 
 # # actual script
-# mkdir -p $PREFIX/lib
-# echo "Linking 'libgomp${SHLIB_EXT}.1' to 'libomp${SHLIB_EXT}'"
-# ln -s $PREFIX/lib/libomp${SHLIB_EXT} $PREFIX/lib/libgomp${SHLIB_EXT}.1
-#
-# echo "Checking link"
-# pushd $PREFIX/lib
-# ls -lah
-# popd
+mkdir -p $PREFIX/lib
+echo "Linking 'libgomp${SHLIB_EXT}.1' to 'libomp${SHLIB_EXT}'"
+ln -s $PREFIX/lib/libomp${SHLIB_EXT} $PREFIX/lib/libgomp${SHLIB_EXT}.1
+
+echo "Checking link"
+pushd $PREFIX/lib
+ls -lah
+popd
