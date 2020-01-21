d5fd849a0a3926e90d0940345e5b3312198b7a40
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-01-21 05:33:16 -0600

    Update build-llvm-openmp.sh

--- recipe/build-llvm-openmp.sh
+++ recipe/build-llvm-openmp.sh
@@ -20,10 +20,6 @@ make -j${CPU_COUNT}
 make install
 
 rm -f $PREFIX/lib/libgomp$SHLIB_EXT
-rm -f $PREFIX/lib/libiomp5$SHLIB_EXT
-
-# make a symlink
-ln -s $PREFIX/lib/libomp$SHLIB_EXT $PREFIX/lib/libiomp5$SHLIB_EXT
 
 mkdir -p $PREFIX/lib/clang/$PKG_VERSION/include
 # Standalone libomp build doesn't put omp.h in clang's default search path
