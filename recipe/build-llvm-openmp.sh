fb6c6c744105f6b994f5f8b97884d56c99fd118d
Isuru Fernando <isuruf@gmail.com>
2021-04-05 22:56:17 -0500

    Move libarcher.so

--- recipe/build-llvm-openmp.sh
+++ recipe/build-llvm-openmp.sh
@@ -18,7 +18,6 @@ cmake ${CMAKE_ARGS} \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=$PREFIX \
-    -DDLIBOMP_OMPT_SUPPORT=1 \
     ..
 
 make -j${CPU_COUNT} VERBOSE=1
@@ -29,3 +28,7 @@ rm -f $PREFIX/lib/libgomp$SHLIB_EXT
 mkdir -p $PREFIX/lib/clang/$PKG_VERSION/include
 # Standalone libomp build doesn't put omp.h in clang's default search path
 cp $PREFIX/include/omp.h $PREFIX/lib/clang/$PKG_VERSION/include
+if [[ "$target_platform" == linux-* ]]; then
+  # move libarcher.so so that it doesn't interfere
+  mv $PREFIX/lib/libarcher.so $PREFIX/lib/libarcher.so.bak
+fi
