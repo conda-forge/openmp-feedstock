3f4d994dfb2d8175726f9949e41611f125d41b99
Isuru Fernando <isuruf@gmail.com>
2023-03-19 14:07:16 -0500

    add comment

--- recipe/build-llvm-openmp.sh
+++ recipe/build-llvm-openmp.sh
@@ -16,6 +16,7 @@ if [[ "${target_platform}" == "linux"* ]]; then
 fi
 
 if [[ "$target_platform" == "linux-ppc64le" ]]; then
+  # avoid error 'relocation truncated to fit: R_PPC64_REL24'
   CFLAGS="$(echo ${CFLAGS} | sed 's/-fno-plt//g')"
   CXXFLAGS="$(echo ${CXXFLAGS} | sed 's/-fno-plt//g')"
 fi
