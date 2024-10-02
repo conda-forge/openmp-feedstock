e4b6e639dcad0d712a77eb577cb7ba8a53398bca
H. Vetinari <h.vetinari@gmx.com>
2024-10-02 15:45:35 +0200

    Revert "remove fortran bits"
    This reverts commit 2b912c00a9fbd73a9e56faff38148ac0754498f0.

--- recipe/build-pkg.sh
+++ recipe/build-pkg.sh
@@ -34,6 +34,8 @@ if [[ "${target_platform}" == "linux"* ]]; then
   # This should have been defined by HandleLLVMOptions.cmake
   # Not sure why it is not.
   export CXXFLAGS="$CXXFLAGS -D__STDC_FORMAT_MACROS"
+  # not possible on osx due to build cycle, see meta.yaml
+  export EXTRA_CMAKE="-DLIBOMP_FORTRAN_MODULES=ON"
 fi
 
 if [[ "${PKG_VERSION}" == *rc* ]]; then
@@ -47,6 +49,7 @@ cmake -G Ninja \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH=$PREFIX \
+    ${EXTRA_CMAKE} \
     ..
 
 cmake --build .
